670234131ec22b9105c64efa90f9a1b0a5985251
martina <martinaromany289@gmail.com>
2025-08-18 00:22:57 +0300

    about section updated

--- src/app/components/sections/About/AboutWeAre.tsx
+++ src/app/components/sections/About/AboutWeAre.tsx
@@ -13,6 +13,19 @@ export default function AboutTeam() {
       <p className="text-blue-400 text-lg md:text-2xl font-semibold max-w-3xl mx-auto">
         What sets us apart is not just what we create, but how we think.
       </p>
+
+      <p className="text-white text-lg md:text-2xl max-w-3xl mx-auto mt-4">
+        From bold brand identities to immersive campaigns, and from high-impact digital solutions to premium print executions, we are relentless in our pursuit of excellence.
+        At our core, we are customer-obsessed creators, dedicated to understanding our clients&apos; unique challenges and transforming them into powerful, purposeful solutions. 
+        Every concept is tailored, every design intentional, and every deliverable executed to perfection.
+      </p>
+      
+      <p className="text-white text-lg md:text-2xl max-w-3xl mx-auto mt-4">
+        We don’t follow trends we build them. 
+        We don’t just communicate we connect.
+        We don’t just deliver we elevate.
+
+      </p>
     </section>
   );
 }
