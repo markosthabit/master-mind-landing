e107f1fd77676be4b9bfdd638f4b2c5acc1e1e7f
markosthabit <markosthabit@protonmail.com>
2025-08-17 23:00:12 +0300

    🐛fix(config): remove tailwind preflight scripts

--- src/app/components/sections/Solutions/SolutionsIntro.tsx
+++ src/app/components/sections/Solutions/SolutionsIntro.tsx
@@ -1,8 +1,8 @@
-// components/sections/Solutions.tsx
 export default function Solutions() {
   return (
     <div className="text-center space-y-4">
-      <h1 className="text-4xl font-bold text-teal-600">Solutions</h1>
+      <h1 className="text-3xl font-bold text-lightBlue">Solutions</h1>
+      <h1 className="text-5xl">This should be 30px</h1>   
       <p className="text-gray-600 max-w-lg mx-auto">
         From digital campaigns to branding strategies, we craft tailored solutions to meet your business goals.
       </p>
